9cb60d391089ab325ec1ab739a8d587d192092e5
Sargam Poudel <sargampoudel100@gmail.com>
2025-03-23 03:09:08 +0530

    rm unwanted db change

--- apps/nextjs/src/db/schema/topics.ts
+++ apps/nextjs/src/db/schema/topics.ts
@@ -23,11 +23,8 @@ export const topicsRelations = relations(topics, ({ many, one }) => ({
   parent: one(topics, {
     fields: [topics.parentId],
     references: [topics.id],
-    relationName: "topic_parent_relation",
   }),
-  children: many(topics, {
-    relationName: "topic_parent_relation",
-  }),
+  children: many(topics),
   mailbox: one(mailboxes, {
     fields: [topics.mailboxId],
     references: [mailboxes.id],
