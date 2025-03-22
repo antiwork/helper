1d7b62645a9d1e21f3573f61dd36fb68280a509b
Jono Mingard <reason.koan@gmail.com>
2025-03-22 23:56:22 +0000

    Fix

--- apps/tauri/src-tauri/src/lib.rs
+++ apps/tauri/src-tauri/src/lib.rs
@@ -8,8 +8,8 @@ use objc2::DefinedClass;
 use objc2::{define_class, msg_send, MainThreadMarker, MainThreadOnly};
 use objc2_authentication_services::{
     ASAuthorization, ASAuthorizationAppleIDCredential, ASAuthorizationAppleIDProvider,
-    ASAuthorizationController, ASAuthorizationControllerDelegate, ASAuthorizationCustomMethod,
-    ASAuthorizationRequest, ASAuthorizationScopeEmail, ASAuthorizationScopeFullName,
+    ASAuthorizationController, ASAuthorizationControllerDelegate, ASAuthorizationRequest,
+    ASAuthorizationScopeEmail, ASAuthorizationScopeFullName,
 };
 use objc2_foundation::{NSArray, NSError, NSObject, NSObjectProtocol};
 use std::cell::RefCell;
@@ -33,11 +33,11 @@ thread_local! {
 fn start_options() -> serde_json::Value {
     let deep_link = DEEP_LINK_URL.lock().unwrap().clone();
 
-    #[cfg(debug_assertions)]
+    // #[cfg(debug_assertions)]
     let is_dev = true;
 
-    #[cfg(not(debug_assertions))]
-    let is_dev = false;
+    // #[cfg(not(debug_assertions))]
+    // let is_dev = false;
 
     serde_json::json!({
         "isDev": is_dev,
@@ -72,11 +72,24 @@ define_class!(
                 .downcast::<ASAuthorizationAppleIDCredential>()
                 .unwrap();
             let code = String::from_utf8(credential.authorizationCode().unwrap().to_vec()).unwrap();
+            let firstName = credential
+                .fullName()
+                .map(|name| name.givenName().unwrap_or_default().to_string());
+            let lastName = credential
+                .fullName()
+                .map(|name| name.familyName().unwrap_or_default().to_string());
             self.ivars()
                 .app
-                .emit("apple-sign-in-complete", code)
+                .emit(
+                    "apple-sign-in-complete",
+                    serde_json::json!({
+                        "code": code,
+                        "firstName": firstName,
+                        "lastName": lastName,
+                    }),
+                )
                 .unwrap();
-            log::info!("Authorization complete: {:?}", code);
+            log::info!("Authorization complete");
         }
 
         #[unsafe(method(authorizationController:didCompleteWithError:))]
@@ -86,7 +99,10 @@ define_class!(
             _controller: &ASAuthorizationController,
             error: &NSError,
         ) {
-            self.ivars().app.emit("apple-sign-in-error").unwrap();
+            self.ivars()
+                .app
+                .emit("apple-sign-in-error", error.code())
+                .unwrap();
             log::error!("Authorization error: {:?}", error);
         }
     }
