6a706d1fb006239fba446b5f9ab5a3611151b1f4
Sargam Poudel <sargampoudel100@gmail.com>
2025-03-23 03:12:34 +0530

    rm unwanted migrations

--- apps/nextjs/src/db/drizzle/0061_unique_hardball.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "mailboxes_mailbox" ALTER COLUMN "auto_close_days_of_inactivity" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "mailboxes_metadataapi" ADD COLUMN "auto_close_enabled" boolean DEFAULT false NOT NULL;--> statement-breakpoint
-ALTER TABLE "mailboxes_metadataapi" ADD COLUMN "auto_close_days_of_inactivity" integer DEFAULT 14 NOT NULL;
--- apps/nextjs/src/db/drizzle/0062_little_scarecrow.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "mailboxes_metadataapi" DROP COLUMN "auto_close_enabled";--> statement-breakpoint
-ALTER TABLE "mailboxes_metadataapi" DROP COLUMN "auto_close_days_of_inactivity";
