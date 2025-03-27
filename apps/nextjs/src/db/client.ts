7eaf95dfbe0d76491ea6185e6b23535596a87854
Jono Mingard <reason.koan@gmail.com>
2025-03-27 21:42:18 +0000

    Fix

--- apps/nextjs/src/db/client.ts
+++ apps/nextjs/src/db/client.ts
@@ -1,14 +1,13 @@
 import { ExtractTablesWithRelations } from "drizzle-orm";
+import { drizzle, NodePgQueryResultHKT } from "drizzle-orm/node-postgres";
 import { PgTransaction } from "drizzle-orm/pg-core";
-import { drizzle, PostgresJsQueryResultHKT } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { Pool, PoolConfig } from "pg";
 import * as schema from "@/db/schema";
 import { env } from "@/env";
 
-export const createDbClient = (url: string, options: postgres.Options<any> = {}) => {
-  // Disable prefetch as it is not supported for "Transaction" pool mode
-  const client = postgres(url, { prepare: false, ...options });
-  return drizzle(client, { schema, casing: "snake_case", logger: !!env.DRIZZLE_LOGGING });
+export const createDbClient = (url: string, options: PoolConfig = {}) => {
+  const pool = new Pool({ connectionString: url, ...options });
+  return drizzle({ client: pool, schema, casing: "snake_case", logger: !!env.DRIZZLE_LOGGING });
 };
 
 type DrizzleClientType = ReturnType<typeof createDbClient>;
@@ -24,10 +23,6 @@ export { db };
 
 if (env.NODE_ENV !== "production") global.drizzleGlobal = db;
 
-export type Transaction = PgTransaction<
-  PostgresJsQueryResultHKT,
-  typeof schema,
-  ExtractTablesWithRelations<typeof schema>
->;
+export type Transaction = PgTransaction<NodePgQueryResultHKT, typeof schema, ExtractTablesWithRelations<typeof schema>>;
 
 export type TransactionOrDb = Transaction | typeof db;
