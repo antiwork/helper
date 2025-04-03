f958937c3fcdf84c11b50c2e79d9edd6749151c6
Raphael Costa <raphael@raphaelcosta.net>
2025-04-03 13:12:53 -0400

    fixes

--- apps/nextjs/src/app/(dashboard)/widget/test/WidgetButtons.tsx
+++ apps/nextjs/src/app/(dashboard)/widget/test/WidgetButtons.tsx
@@ -41,10 +41,13 @@ export function WidgetButtons() {
         >
           Toggle widget (data-attr)
         </button>
+      </div>
 
+      <div className="space-y-2 mt-6">
+        <h3 className="text-sm font-medium text-white">Change refund policy to 3 months:</h3>
         <button
           data-helper-start-guide="Change my refund policy to 3 months - Go to the Settings page, scroll down to the Refund Policy section, and select a 3 months refund policy from the dropdown menu. If needed, you can also add fine print to describe your policy in more detail."
-          className="ml-4 focus:shadow-outline rounded bg-blue-500 px-4 py-2 font-bold text-primary-foreground transition duration-300 hover:bg-blue-600 focus:outline-none"
+          className="focus:shadow-outline rounded bg-blue-500 px-4 py-2 font-bold text-primary-foreground transition duration-300 hover:bg-blue-600 focus:outline-none"
         >
           Start guide (data-attr)
         </button>
