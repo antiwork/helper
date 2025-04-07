ee112365630a3833fa18f24011634448380947b8
Shubhdeep Chhabra <chhabrashubhdeep@gmail.com>
2025-04-07 17:41:39 +0530

    Update apps/nextjs/sentry.common.config.ts
    Co-authored-by: Jono M <reason.koan@gmail.com>
    Signed-off-by: Shubhdeep Chhabra <chhabrashubhdeep@gmail.com>

--- apps/nextjs/sentry.common.config.ts
+++ apps/nextjs/sentry.common.config.ts
@@ -1,7 +1,7 @@
 import { env } from "@/env";
 
 export const commonConfig = {
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  dsn: env.NEXT_PUBLIC_SENTRY_DSN,
   enabled: env.NODE_ENV === "production",
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
