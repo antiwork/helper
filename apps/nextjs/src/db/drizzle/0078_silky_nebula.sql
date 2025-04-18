ed9d158669b5a8344c3c39d4ae855f4ba64eb8f5
Jono Mingard <reason.koan@gmail.com>
2025-04-18 16:58:35 +0100

    Fix

--- apps/nextjs/src/db/drizzle/0078_silky_nebula.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "agent_messages_message" DROP CONSTRAINT "agent_messages_agent_thread_id_fkey";
---> statement-breakpoint
-ALTER TABLE "agent_threads_thread" DROP CONSTRAINT "agent_threads_mailbox_id_fkey";
