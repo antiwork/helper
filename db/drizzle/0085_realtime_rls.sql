62ffbf9f4efef161f598cef0c1e956483639ce47
Jono Mingard <reason.koan@gmail.com>
2025-05-21 23:29:49 +0100

    Fix

--- db/drizzle/0085_realtime_rls.sql
+++ db/drizzle/0085_realtime_rls.sql
@@ -1,14 +1,20 @@
 -- Custom SQL migration file, put your code below! --
 DO $$
 BEGIN
-  IF NOT EXISTS (
-    SELECT 1 FROM pg_policies 
-    WHERE schemaname = 'realtime' 
-      AND tablename = 'messages' 
-      AND policyname = 'Allow listening for broadcasts for authenticated users only'
+  IF EXISTS (
+    SELECT 1 FROM pg_tables
+    WHERE schemaname = 'realtime'
+      AND tablename = 'messages'
   ) THEN
-    EXECUTE 'CREATE POLICY "Allow listening for broadcasts for authenticated users only" ON "realtime"."messages" AS PERMISSIVE FOR SELECT TO authenticated USING (realtime.messages.extension = ''broadcast'')';
-  ELSE
-    EXECUTE 'ALTER POLICY "Allow listening for broadcasts for authenticated users only" ON "realtime"."messages" TO authenticated USING (realtime.messages.extension = ''broadcast'')';
+    IF NOT EXISTS (
+      SELECT 1 FROM pg_policies
+      WHERE schemaname = 'realtime'
+        AND tablename = 'messages'
+        AND policyname = 'Allow listening for broadcasts for authenticated users only'
+    ) THEN
+      EXECUTE 'CREATE POLICY "Allow listening for broadcasts for authenticated users only" ON "realtime"."messages" AS PERMISSIVE FOR SELECT TO authenticated USING (realtime.messages.extension = ''broadcast'')';
+    ELSE
+      EXECUTE 'ALTER POLICY "Allow listening for broadcasts for authenticated users only" ON "realtime"."messages" TO authenticated USING (realtime.messages.extension = ''broadcast'')';
+    END IF;
   END IF;
 END$$;
