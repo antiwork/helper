830965ffa52c57c63ed89cee21858b0fff8d99e9
Jono Mingard <reason.koan@gmail.com>
2025-06-02 01:17:53 +0100

    Fix

--- db/lib/encryptedField.ts
+++ db/lib/encryptedField.ts
@@ -1,37 +1,16 @@
 import { customType } from "drizzle-orm/pg-core";
 import { symmetricDecrypt, symmetricEncrypt } from "@/db/lib/crypto";
-import { DjangoCryptoCompat } from "@/db/lib/djangoCrypto";
 import { env } from "@/lib/env";
 
-const secretKey = env.CRYPTO_SECRET;
 const nativeEncryptColumnSecret = env.ENCRYPT_COLUMN_SECRET;
-const djangoCrypto = new DjangoCryptoCompat(secretKey);
 
-export const encryptedField = customType<{ data: string }>({
+export const bytea = customType<{ data: Buffer; notNull: false; default: false }>({
   dataType() {
     return "bytea";
   },
-
-  toDriver(value: string): Buffer {
-    return djangoCrypto.encrypt(value);
-  },
-
-  fromDriver(value: unknown): string {
-    let bufferValue: Buffer;
-    if (typeof value === "string") {
-      bufferValue = Buffer.from(value.slice(2), "hex"); // Remove '\x' prefix
-    } else if (Buffer.isBuffer(value)) {
-      bufferValue = value;
-    } else {
-      throw new Error(`Unexpected value type: ${typeof value}`);
-    }
-
-    const decrypted = djangoCrypto.decrypt(bufferValue);
-    return decrypted.toString("utf-8");
-  },
 });
 
-export const nativeEncryptedField = customType<{ data: string }>({
+export const encryptedField = customType<{ data: string }>({
   dataType() {
     return "bytea";
   },
