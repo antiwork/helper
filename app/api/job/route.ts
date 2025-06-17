6c6101363b85c206d04350db2e41e8db404345cc
Jono Mingard <reason.koan@gmail.com>
2025-06-17 20:21:58 +0100

    Fix

--- app/api/job/route.ts
+++ app/api/job/route.ts
@@ -1,9 +1,15 @@
 import { createHmac, timingSafeEqual } from "crypto";
 import { waitUntil } from "@vercel/functions";
+import { eq, sql } from "drizzle-orm";
 import { NextRequest } from "next/server";
+import { takeUniqueOrThrow } from "@/components/utils/arrays";
+import { assertDefined } from "@/components/utils/assert";
+import { db } from "@/db/client";
+import { jobRuns } from "@/db/schema";
 import { cronJobs, eventJobs } from "@/inngest/functions";
 import { EventData, EventName } from "@/inngest/utils";
 import { env } from "@/lib/env";
+import { captureExceptionAndLog } from "@/lib/shared/sentry";
 
 const verifyHmac = (body: string, providedHmac: string): boolean => {
   try {
@@ -11,6 +17,36 @@ const verifyHmac = (body: string, providedHmac: string): boolean => {
     return timingSafeEqual(Buffer.from(providedHmac, "hex"), Buffer.from(expectedHmac, "hex"));
   } catch {
     return false;
+  }
+};
+
+const retrySeconds: Record<number, number> = {
+  0: 5,
+  1: 60,
+  2: 5 * 60,
+  3: 60 * 60,
+};
+
+const handleJob = async (jobRun: typeof jobRuns.$inferSelect, handler: Promise<any>) => {
+  try {
+    const result = await handler;
+    await db.update(jobRuns).set({ status: "success", result }).where(eq(jobRuns.id, jobRun.id));
+  } catch (error) {
+    captureExceptionAndLog(error);
+    await db.transaction(async (tx) => {
+      if (retrySeconds[jobRun.attempts]) {
+        const payload = { job: jobRun.job, data: jobRun.data, event: jobRun.event, jobRunId: jobRun.id };
+        await tx.execute(sql`pgmq_public.send('jobs', ${payload}, ${retrySeconds[jobRun.attempts]})`);
+      }
+      await tx
+        .update(jobRuns)
+        .set({
+          status: "error",
+          error: error instanceof Error ? error.message : `${error}`,
+          attempts: jobRun.attempts + 1,
+        })
+        .where(eq(jobRuns.id, jobRun.id));
+    });
   }
 };
 
@@ -28,21 +64,33 @@ export const POST = async (request: NextRequest) => {
       return new Response("Unauthorized", { status: 401 });
     }
 
-    const data = JSON.parse(body) as { event: EventName; job: string; data: EventData<EventName> } | { job: string };
+    const data = JSON.parse(body) as
+      | { event: EventName; job: string; jobRunId?: number; data: EventData<EventName> }
+      | { job: string; jobRunId?: number; event?: undefined; data?: undefined };
 
-    if ("event" in data) {
+    const jobRun = data.jobRunId
+      ? assertDefined(await db.query.jobRuns.findFirst({ where: eq(jobRuns.id, data.jobRunId) }))
+      : await db
+          .insert(jobRuns)
+          .values({ job: data.job, event: data.event, data: data.data ?? null })
+          .returning()
+          .then(takeUniqueOrThrow);
+
+    if (data.event) {
       const jobs = eventJobs[data.event].jobs;
       const handler = jobs[data.job as keyof typeof jobs] as (data: EventData<EventName>) => Promise<any>;
       if (!handler) {
+        await db.update(jobRuns).set({ status: "error", error: "Job not found" }).where(eq(jobRuns.id, jobRun.id));
         return new Response("Not found", { status: 404 });
       }
-      waitUntil(handler(data.data));
+      waitUntil(handleJob(jobRun, handler(data.data)));
     } else {
       const handler = Object.assign({}, ...Object.values(cronJobs))[data.job] as () => Promise<any>;
       if (!handler) {
+        await db.update(jobRuns).set({ status: "error", error: "Job not found" }).where(eq(jobRuns.id, jobRun.id));
         return new Response("Not found", { status: 404 });
       }
-      waitUntil(handler());
+      waitUntil(handleJob(jobRun, handler()));
     }
 
     return new Response("OK");
