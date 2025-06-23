9a54526cb6da22e6877f1f8032cb087afc54ecc6
Adarsh Tiwari <adarshtiwari797023@gmail.com>
2025-06-23 15:03:55 +0530

    fix

--- app/(dashboard)/mailboxes/[mailbox_slug]/appSidebar.tsx
+++ app/(dashboard)/mailboxes/[mailbox_slug]/appSidebar.tsx
@@ -183,44 +183,40 @@ export function AppSidebar({ mailboxSlug }: { mailboxSlug: string }) {
                   </Link>
                 </SidebarMenuButton>
               </SidebarMenuItem> */}
-              <SidebarGroup>
-                <SidebarMenu>
-                  <Collapsible open={open} onOpenChange={setOpen}>
-                    <SidebarMenuItem>
-                      <CollapsibleTrigger asChild>
-                        <SidebarMenuButton>
-                          <SettingsIcon className="size-4" />
-                          <span className="group-data-[collapsible=icon]:hidden">Settings</span>
-                          {open ? (
-                            <ChevronDown className="ml-auto h-4 w-4" />
-                          ) : (
-                            <ChevronRight className="ml-auto h-4 w-4" />
-                          )}
+              <Collapsible open={open} onOpenChange={setOpen}>
+                <SidebarMenuItem>
+                  <CollapsibleTrigger asChild>
+                    <SidebarMenuButton>
+                      <SettingsIcon className="size-4" />
+                      <span className="group-data-[collapsible=icon]:hidden">Settings</span>
+                      {open ? (
+                        <ChevronDown className="ml-auto h-4 w-4" />
+                      ) : (
+                        <ChevronRight className="ml-auto h-4 w-4" />
+                      )}
+                    </SidebarMenuButton>
+                  </CollapsibleTrigger>
+                </SidebarMenuItem>
+      
+                <CollapsibleContent asChild>
+                  <div className="ml-4 mt-1 space-y-1">
+                    {settingsItems.map((item) => (
+                      <SidebarMenuItem key={item.id}>
+                        <SidebarMenuButton
+                          asChild
+                          isActive={pathname === `/mailboxes/gumroad/settings/${item.id}`}
+                          className="pl-2"
+                        >
+                          <Link href={`/mailboxes/gumroad/settings/${item.id}`}>
+                            {<item.icon className="size-4" />}
+                            <span className="group-data-[collapsible=icon]:hidden">{item.label}</span>
+                          </Link>
                         </SidebarMenuButton>
-                      </CollapsibleTrigger>
-                    </SidebarMenuItem>
-          
-                    <CollapsibleContent asChild>
-                      <div className="ml-4 mt-1 space-y-1">
-                        {settingsItems.map((item) => (
-                          <SidebarMenuItem key={item.id}>
-                            <SidebarMenuButton
-                              asChild
-                              isActive={pathname === `/mailboxes/gumroad/settings/${item.id}`}
-                              className="pl-2"
-                            >
-                              <Link href={`/mailboxes/gumroad/settings/${item.id}`}>
-                                {<item.icon className="size-4" />}
-                                <span className="group-data-[collapsible=icon]:hidden">{item.label}</span>
-                              </Link>
-                            </SidebarMenuButton>
-                          </SidebarMenuItem>
-                        ))}
-                      </div>
-                    </CollapsibleContent>
-                  </Collapsible>
-                </SidebarMenu>
-              </SidebarGroup>
+                      </SidebarMenuItem>
+                    ))}
+                  </div>
+                </CollapsibleContent>
+              </Collapsible>
             </SidebarMenu>
           </SidebarGroup>
         </div>
