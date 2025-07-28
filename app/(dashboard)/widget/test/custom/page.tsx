495bfda2c16e604a858184d0c43690d073a5c24e
Jono Mingard <reason.koan@gmail.com>
2025-07-28 23:49:22 +0100

    Fix

--- app/(dashboard)/widget/test/custom/page.tsx
+++ app/(dashboard)/widget/test/custom/page.tsx
@@ -1,7 +1,7 @@
 import { CustomWidgetTest } from "@/app/(dashboard)/widget/test/custom/customWidgetTest";
 import { generateSession } from "@/app/(dashboard)/widget/test/custom/generateSession";
-import { HelperClientProvider } from "@/app/(dashboard)/widget/test/custom/helperClientProvider";
 import { getBaseUrl } from "@/components/constants";
+import { HelperClientProvider } from "@/packages/react/src/components/helperClientProvider";
 
 export const dynamic = "force-dynamic";
 
