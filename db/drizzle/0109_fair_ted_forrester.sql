464f8851088274db3e3ee427fd4d9668f1cb5faa
Anshgrover23 <ag5989670@gmail.com>
2025-08-05 01:06:12 +0530

    add seperate migrations for add and rename columns

--- db/drizzle/0109_fair_ted_forrester.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE "tool_apis" ALTER COLUMN "authentication_token" SET DATA TYPE text;--> statement-breakpoint
-ALTER TABLE "tools" ALTER COLUMN "authentication_token" SET DATA TYPE text;--> statement-breakpoint
-ALTER TABLE "messages" ADD COLUMN "body" text;--> statement-breakpoint
-ALTER TABLE "messages" ADD COLUMN "cleaned_up_text" text;--> statement-breakpoint
-ALTER TABLE "conversations_conversation" ADD COLUMN "subject" text;--> statement-breakpoint
-ALTER TABLE "mailboxes_gmailsupportemail" ADD COLUMN "access_token" text;--> statement-breakpoint
-ALTER TABLE "mailboxes_gmailsupportemail" ADD COLUMN "refresh_token" text;--> statement-breakpoint
-ALTER TABLE "tool_apis" ADD COLUMN "encrypted_authentication_token" "bytea";--> statement-breakpoint
-ALTER TABLE "tools" ADD COLUMN "encrypted_authentication_token" "bytea";
