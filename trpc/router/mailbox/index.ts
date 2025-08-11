48ab0bb8e17a6d2b282cd686a3ebbb15d05422c2
Udit Takkar <udit222001@gmail.com>
2025-08-11 14:54:00 +0530

    chore: remove E2E tests and dashboard page

--- trpc/router/mailbox/index.ts
+++ trpc/router/mailbox/index.ts
@@ -4,7 +4,6 @@ import { z } from "zod";
 import { db } from "@/db/client";
 import { conversations, mailboxes } from "@/db/schema";
 import { triggerEvent } from "@/jobs/trigger";
-import { getLatestEvents } from "@/lib/data/dashboardEvent";
 import { getGuideSessionsForMailbox } from "@/lib/data/guide";
 import { getMailboxInfo } from "@/lib/data/mailbox";
 import { conversationsRouter } from "./conversations/index";
@@ -80,9 +79,6 @@ export const mailboxRouter = {
         .where(eq(mailboxes.id, ctx.mailbox.id));
     }),
 
-  latestEvents: mailboxProcedure
-    .input(z.object({ cursor: z.date().optional() }))
-    .query(({ ctx, input }) => getLatestEvents(ctx.mailbox, input.cursor)),
 
   getSessionsPaginated: mailboxProcedure
     .input(
