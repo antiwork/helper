14a658895e33c5b17ae399b630a0a31cfed8cfc1
12private12 <120792mon@gmail.com>
2025-08-11 18:36:16 +0800

    Delete tests directory
    Signed-off-by: 12private12 <120792mon@gmail.com>

--- tests/e2e/utils/toastHelpers.ts
+++
@@ -1,6 +0,0 @@
-import { expect, Page } from "@playwright/test";
-
-export async function waitForToast(page: Page, message: string) {
-  const toast = page.locator("[data-sonner-toast]").filter({ hasText: message });
-  await expect(toast.first()).toBeVisible({ timeout: 8000 });
-}
