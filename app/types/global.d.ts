eec950dc20da581cff07b951c57d0ecc3e715bf1
MuskanPaliwal <muskan10112002@gmail.com>
2025-08-15 09:18:44 +0530

    add type and ui component for the badge

--- app/types/global.d.ts
+++ app/types/global.d.ts
@@ -25,4 +25,5 @@ export type AttachedFile = Message["files"][number];
 
 export type ConversationListItem = RouterOutputs["mailbox"]["conversations"]["list"]["conversations"][number] & {
   assignedToAI?: boolean;
+  unreadMessageCount?: number;
 };
