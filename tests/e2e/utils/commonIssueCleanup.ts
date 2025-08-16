cea1f33f5548c89461fde0fdf33f582db1fef513
Udit Takkar <udit222001@gmail.com>
2025-08-16 22:55:59 +0530

    chore: remove comments

--- tests/e2e/utils/commonIssueCleanup.ts
+++ tests/e2e/utils/commonIssueCleanup.ts
@@ -2,26 +2,10 @@ import { eq, inArray } from "drizzle-orm";
 import { db } from "../../../db/client";
 import { issueGroups } from "../../../db/schema";
 
-/**
- * Deletes common issues from the database by title
- * @param titles - Array of issue titles to delete
- * @returns Promise<void>
- */
 export async function deleteCommonIssuesFromDb(titles: string[]): Promise<void> {
   try {
     if (titles.length > 0) {
       await db.delete(issueGroups).where(inArray(issueGroups.title, titles));
     }
-  } catch (error) {
-    // Don't fail the test suite due to cleanup issues
-  }
-}
-
-/**
- * Deletes a single common issue from the database by title
- * @param title - Issue title to delete
- * @returns Promise<void>
- */
-export async function deleteCommonIssueFromDb(title: string): Promise<void> {
-  await deleteCommonIssuesFromDb([title]);
+  } catch (error) {}
 }
