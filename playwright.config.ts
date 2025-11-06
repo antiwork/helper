fc13ae5c550aa2ea09daf088365df0bcb95d23fb
stefan binoj <stefan.binoj.007@gmail.com>
2025-11-06 20:33:11 +0530

    added github reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   retries: process.env.CI ? 1 : 0,
   workers: process.env.CI ? "100%" : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? [["list"], ["html"]] : "list",
+  reporter: process.env.CI ? [["list"], ["html"], ["github"]] : "list",
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
